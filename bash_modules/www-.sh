2573b90ae5f05dcf550894faf2b8edce6923903e
tim.higham@liveinfo247.com <tim.higham@liveinfo247.com>
2023-07-02 12:13:21 +0000

    update

--- bash_modules/www-.sh
+++ bash_modules/www-.sh
@@ -8,3 +8,58 @@ function www-showsites() {
 	done
 	echo ""
 }
+
+function www-setsites() {
+	echo-hr
+	echo "Available site directories:"
+	ls $wwwroot/html
+	www-showsites
+	echo "Enter site number to change"
+	read option
+	sitenumber=$(($option - 1))
+	echo "Enter site root directory name to set against site $option"
+	read dir
+	wwwsites[$sitenumber]=$dir
+	if [ -d "$dir" ]; then #just change option if repo exists
+		echo "Setting $option to $dir"
+		www_sitefocus=$dir
+	else    # need to set up repo
+		set -e #stop everything if there is a failure
+		echo "Directory $dir not found."
+		echo "Will install under $wwwroot/html/$dir"
+		echo "Please enter the git reponame to put here"
+		read reponame
+		echo "Installing '$reponame' under $wwwroot/html/$dir"
+		git-installrepo $dir $reponame
+
+	fi
+	cd "$wwwroot/html/$www_sitefocus"
+	echo "setting site to $www_sitefocus"
+	bash-writesettings
+	www-switch
+}
+
+function www-switch() {
+	www-showsites
+	echo "Please select a site number to chose for operations"
+	read sitenumber
+	echo "Git will auto sync. Enter 'n' to prevent this"
+	read -t 3 input
+	if [ "$input" != "n" ]; then
+		git-push
+	fi
+	sitenumber=$((sitenumber - 1))
+	www_sitefocus=${wwwsites[sitenumber]}
+	cd "$wwwroot/html/$www_sitefocus"
+	echo "setting site to $www_sitefocus"
+	bash-writesettings
+	if [ "$input" != "n" ]; then
+		git-pull
+	fi
+	bash-start
+}
+
+function www-routes() {
+	php artisan route:list
+}
+
--- bashinstall/bash_profile_user.sh
+++ bashinstall/bash_profile_user.sh
@@ -270,35 +270,7 @@ function bash-logout() {
 	source ~/.bash_profile
 }
 
-function www-setsites() {
-	echo-hr
-	echo "Available site directories:"
-	ls $wwwroot/html
-	www-showsites
-	echo "Enter site number to change"
-	read option
-	sitenumber=$(($option - 1))
-	echo "Enter site root directory name to set against site $option"
-	read dir
-	wwwsites[$sitenumber]=$dir
-	if [ -d "$dir" ]; then #just change option if repo exists
-		echo "Setting $option to $dir"
-		www_sitefocus=$dir
-	else    # need to set up repo
-		set -e #stop everything if there is a failure
-		echo "Directory $dir not found."
-		echo "Will install under $wwwroot/html/$dir"
-		echo "Please enter the git reponame to put here"
-		read reponame
-		echo "Installing '$reponame' under $wwwroot/html/$dir"
-		git-installrepo $dir $reponame
 
-	fi
-	cd "$wwwroot/html/$www_sitefocus"
-	echo "setting site to $www_sitefocus"
-	bash-writesettings
-	www-switch
-}
 
 function git-installrepo() {
 	env-attributerequire gituname
@@ -327,27 +299,6 @@ function git-installrepo() {
 		if [ "$dotenv" != "" ]; then
 		  echo "dotenv";
 	fi
-}
-
-
-function www-switch() {
-	www-showsites
-	echo "Please select a site number to chose for operations"
-	read sitenumber
-	echo "Git will auto sync. Enter 'n' to prevent this"
-	read -t 3 input
-	if [ "$input" != "n" ]; then
-		git-push
-	fi
-	sitenumber=$((sitenumber - 1))
-	www_sitefocus=${wwwsites[sitenumber]}
-	cd "$wwwroot/html/$www_sitefocus"
-	echo "setting site to $www_sitefocus"
-	bash-writesettings
-	if [ "$input" != "n" ]; then
-		git-pull
-	fi
-	bash-start
 }
 
 function ~www() {
@@ -404,10 +355,6 @@ function cd~() {
 function ~log-sys() {
 	echo "/var/log"
 	ls -al /var/log
-}
-
-function www-routes() {
-	php artisan route:list
 }
 
 function git-setup() {
