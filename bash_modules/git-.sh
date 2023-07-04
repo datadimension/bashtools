a66d2a7bbcb363f3b505e048527f2ed1b79c0cb0
tim.higham@liveinfo247.com <tim.higham@liveinfo247.com>
2023-07-04 17:38:02 +0000

    update

--- bash_modules/git-.sh
+++ bash_modules/git-.sh
@@ -22,3 +22,53 @@ function git-installrepo() {
 	sudo mkdir -p $wwwroot/html/$dir/bootstrap/cache
 	sudo mkdir -p $wwwroot/html/$dir/public/downloads/
 }
+
+function git-pull() {
+	clear
+	echo-hr
+	curpwd=$(pwd)
+	echo-h1 "Pulling to $www_sitefocus"
+	echo "Enter a repo name"
+	echo ""
+	echo "1: DD_libwww"
+	echo "2: DD_laravelAp"
+	echo "3: DD_laraview"
+	echo "or wait / hit enter for everything"
+	read -t 3 option
+	if [ "$option" == "1" ]; then
+		git-pull-repo "DD_libwww"
+	elif [ "$option" == "2" ]; then
+		git-pull-repo "DD_laravelAp"
+	elif [ "$option" == "3" ]; then
+		git-pull-repo "DD_laraview"
+	else
+		git-pull-all
+	fi
+	cd $curpwd
+}
+
+function git-push() {
+	clear
+	curpwd=$(pwd)
+	echo-h1 "Pushing from $www_sitefocus"
+	echo "Enter a repo name"
+	echo ""
+	echo "1: DD_libwww"
+	echo "2: DD_laravelAp"
+	echo "3: DD_laraview"
+	echo "or wait / hit enter for everything"
+	read -t 3 option
+	if [ "$option" == "1" ]; then
+		git-push-repo "DD_libwww"
+		git-pull-repo "DD_libwww"
+	elif [ "$option" == "2" ]; then
+		git-push-repo "DD_laravelAp"
+		git-pull-repo "DD_laravelAp"
+	elif [ "$option" == "3" ]; then
+		git-push-repo "DD_laraview"
+		git-pull-repo "DD_laraview"
+	else
+		git-push-all
+	fi
+	cd $curpwd
+}
--- bashinstall/bash_profile_user.sh
+++ bashinstall/bash_profile_user.sh
@@ -189,31 +189,6 @@ function git-push-all() {
 	git-push-repo "$www_sitefocus"
 }
 
-function git-push() {
-	clear
-	curpwd=$(pwd)
-	echo-h1 "Pushing from $www_sitefocus"
-	echo "Enter a repo name"
-	echo ""
-	echo "1: DD_libwww"
-	echo "2: DD_laravelAp"
-	echo "3: DD_laraview"
-	echo "or wait / hit enter for everything"
-	read -t 3 option
-	if [ "$option" == "1" ]; then
-		git-push-repo "DD_libwww"
-		git-pull-repo "DD_libwww"
-	elif [ "$option" == "2" ]; then
-		git-push-repo "DD_laravelAp"
-		git-pull-repo "DD_laravelAp"
-	elif [ "$option" == "3" ]; then
-		git-push-repo "DD_laraview"
-		git-pull-repo "DD_laraview"
-	else
-		git-push-all
-	fi
-	cd $curpwd
-}
 
 function git-push-repo() {
 	gitreponame=$1
@@ -272,30 +247,6 @@ function git-pull-all() {
 	git-pull-repo "DD_libwww"
 	git-pull-repo "DD_laravelAp"
 	git-pull-repo "$www_sitefocus"
-}
-
-function git-pull() {
-	clear
-	echo-hr
-	curpwd=$(pwd)
-	echo-h1 "Pulling to $www_sitefocus"
-	echo "Enter a repo name"
-	echo ""
-	echo "1: DD_libwww"
-	echo "2: DD_laravelAp"
-	echo "3: DD_laraview"
-	echo "or wait / hit enter for everything"
-	read -t 3 option
-	if [ "$option" == "1" ]; then
-		git-pull-repo "DD_libwww"
-	elif [ "$option" == "2" ]; then
-		git-pull-repo "DD_laravelAp"
-	elif [ "$option" == "3" ]; then
-		git-pull-repo "DD_laraview"
-	else
-		git-pull-all
-	fi
-	cd $curpwd
 }
 
 function ls-i() {
