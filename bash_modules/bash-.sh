be8b64845374f308211840f882f1a0cbc1a26276
Tim H <tim.higham@liveinfo247.com>
2025-02-25 17:14:29 +0000

    update

--- bash_modules/bash-.sh
+++ bash_modules/bash-.sh
@@ -1,235 +1,235 @@
 #!/usr/bin/env bash
 
-function bash-start(){
-	#load modules optimised for all os
-	source ~/bashtools/bash_modules/std-.sh #standard for a platforms
-    source ~/bashtools/bash_modules/env-.sh
-    source ~/bashtools/bash_modules/www-.sh
-	bash-readsettings
-	env-attributerequire "welcomemsg"
-	if [ "$platform" == "ubuntu" ]; then
-    	bash-start-ubuntu
-    else
-    	bash-start-windows
-    fi
+function bash-start() {
+  #load modules optimised for all os
+  source ~/bashtools/bash_modules/std-.sh #standard for a platforms
+  source ~/bashtools/bash_modules/env-.sh
+  source ~/bashtools/bash_modules/www-.sh
+  bash-readsettings
+  env-attributerequire "welcomemsg"
+  if [ "$platform" == "ubuntu" ]; then
+    bash-start-ubuntu
+  else
+    bash-start-windows
+  fi
 }
 
 function bash-start-windows() {
-	    source ~/bashtools/bash_modules_windows/git-.sh
-	clear
-		echo $welcomemsg
-	echo "Welcome to Bash for Windows !"
-		www-reposhow
-	startdir="$wwwroot/html/$www_repofocus"
-    cd $startdir
+  source ~/bashtools/bash_modules_windows/git-.sh
+  clear
+  echo $welcomemsg
+  echo "Welcome to Bash for Windows !"
+  www-reposhow
+  startdir="$wwwroot/html/$www_repofocus"
+  cd $startdir
 }
 
-bash-start-ubuntu-osconfigcheck(){
-		mysql-getversion;
-    	osinstall=0;
-    	os-checkstatus
-    	if [ $osinstall == 1 ]; then
-    		read -p "Any key to restart" wait;
-    		bash-restart
-    	fi
-    	env-attributerequire "serverid"
-    	env-attributerequire "environment"
-    	env-attributerequire "wwwroot"
-    	env-attributerequire "defaultDatabaseIP"
+bash-start-ubuntu-osconfigcheck() {
+  mysql-getversion
+  osinstall=0
+  os-checkstatus
+  if [ $osinstall == 1 ]; then
+    read -p "Any key to restart" wait
+    bash-restart
+  fi
+  env-attributerequire "serverid"
+  env-attributerequire "environment"
+  env-attributerequire "wwwroot"
+  env-attributerequire "defaultDatabaseIP"
 
-    	MENUCHOICE="" #reserved as a global for menu function
-    	PHP_VERSION=`php -r 'echo PHP_VERSION;'`
-    	#env-attributerequire "welcomemsg"
+  MENUCHOICE="" #reserved as a global for menu function
+  PHP_VERSION=$(php -r 'echo PHP_VERSION;')
+  #env-attributerequire "welcomemsg"
 }
 
 # initialises the bash shell #
 function bash-start-ubuntu() {
-    source ~/bashtools/bash_modules/os-.sh
-     source ~/bashtools/bash_modules/mysql-.sh
-        	source ~/bashtools/bash_modules/fsys-.sh
-        	source ~/bashtools/bash_modules/nginx-.sh
-        	source ~/bashtools/bash_modules/php-.sh
-        	    source ~/bashtools/bash_modules/git-.sh
-        	source ~/bashtools/bash_modules/net-.sh
-        	source ~/bashtools/bash_modules/log-.sh
-        	source ~/bashtools/bash_modules/laravel-.sh
-	bash-start-ubuntu-osconfigcheck
-	echo "Welcome to"
-	echo-h1 $serverid
-	echo $welcomemsg
-	echo "Your session IP detected as:"
-  	echo $SSH_CLIENT | awk '{ print $1}'
-	# sudo /etc/init.d/cron start;
-	echo "Use 'env-about' for more info, 'bash-help' for more functions"
-	www-reposhow
-	#bash-showsettings
-	startdir="$wwwroot/html/$www_repofocus"
-	cd $startdir
-	echo ""
-	#laravel-showversion;
-	echo "Recommended URL"
-	echo "$www_repofocus.$serverid.com"
-	echo ""
-	file-showdir $startdir
-	#net-ssh-log-session
-	#laravel-configcheck
+  source ~/bashtools/bash_modules/os-.sh
+  source ~/bashtools/bash_modules/mysql-.sh
+  source ~/bashtools/bash_modules/fsys-.sh
+  source ~/bashtools/bash_modules/nginx-.sh
+  source ~/bashtools/bash_modules/php-.sh
+  source ~/bashtools/bash_modules/git-.sh
+  source ~/bashtools/bash_modules/net-.sh
+  source ~/bashtools/bash_modules/log-.sh
+  source ~/bashtools/bash_modules/laravel-.sh
+  bash-start-ubuntu-osconfigcheck
+  echo "Welcome to"
+  echo-h1 $serverid
+  echo $welcomemsg
+  echo "Your session IP detected as:"
+  echo $SSH_CLIENT | awk '{ print $1}'
+  # sudo /etc/init.d/cron start;
+  echo "Use 'env-about' for more info, 'bash-help' for more functions"
+  www-reposhow
+  #bash-showsettings
+  startdir="$wwwroot/html/$www_repofocus"
+  cd $startdir
+  echo ""
+  #laravel-showversion;
+  echo "Recommended URL"
+  echo "$www_repofocus.$serverid.com"
+  echo ""
+  file-showdir $startdir
+  #net-ssh-log-session
+  #laravel-configcheck
 }
 
-function bash-push(){
-	git-push-repo bashtools
-	~www
+function bash-push() {
+  git-push-repo bashtools
+  ~www
 }
 
-function bash-pull(){
-	echo "updating bash";
-	git-pull-repo bashtools
-	bash-install
+function bash-pull() {
+  echo "updating bash"
+  git-pull-repo bashtools
+  bash-install
 }
 
 # installs the enhanced bash functionality provided by Bashtools
 function bash-install() {
-	 rm ~/.bash_profile
-	mkdir -p ~/bashtoolscfg/tmp
-	homepath=${HOME:0:6}
-	#detect ubuntu or MINW64 - keeping seperate functions for future proofing, even though very similar
-	if [ "$homepath" == "/home/" ]; then
-		bash-install-ubuntu
-	else
-		bash-install-windows
-	fi
+  rm ~/.bash_profile
+  mkdir -p ~/bashtoolscfg/tmp
+  homepath=${HOME:0:6}
+  #detect ubuntu or MINW64 - keeping seperate functions for future proofing, even though very similar
+  if [ "$homepath" == "/home/" ]; then
+    bash-install-ubuntu
+  else
+    bash-install-windows
+  fi
 }
 
-function bash-install-ubuntu(){
-    platform="ubuntu"
-	username=${HOME:6}
-	wwwroot="/var/www"
-    echo -e "Detected:\nPlatform=$platform\nUser=$username\nwwwroot=$wwwroot"
-	cp ~/bashtools/templates/bash/bash_profile.sh ~/.bash_profile #overwrite with potention changes
-   bash-writesettings
-		source ~/.bash_profile
+function bash-install-ubuntu() {
+  platform="ubuntu"
+  username=${HOME:6}
+  wwwroot="/var/www"
+  echo -e "Detected:\nPlatform=$platform\nUser=$username\nwwwroot=$wwwroot"
+  cp ~/bashtools/templates/bash/bash_profile.sh ~/.bash_profile #overwrite with potention changes
+  bash-writesettings
+  source ~/.bash_profile
 }
 
-function bash-install-windows(){
-		platform="windows"
-		username=${HOME:9}
-		wwwroot="${HOME}/www"
-    	echo -e "Detected:\nPlatform=$platform\nUser=$username\nwwwroot=$wwwroot"
-		cp ~/bashtools/templates/bash/bash_profile.sh ~/.bash_profile #overwrite with potention changes
- 	   bash-writesettings
-		source ~/.bash_profile
+function bash-install-windows() {
+  platform="windows"
+  username=${HOME:9}
+  wwwroot="${HOME}/www"
+  echo -e "Detected:\nPlatform=$platform\nUser=$username\nwwwroot=$wwwroot"
+  cp ~/bashtools/templates/bash/bash_profile.sh ~/.bash_profile #overwrite with potention changes
+  bash-writesettings
+  source ~/.bash_profile
 }
 
 # shows bash function categories and functions
 function bash-help() {
-	std-menu std,bash,env,fsys,git,log,net,os,nginx,php,www "Help Categories:"
-	echo "Loading help for $MENUCHOICE ...";
-	php ~/bashtools/php_helpers/bash/bash-help.php helptype=$MENUCHOICE
+  std-menu std,bash,env,fsys,git,log,net,os,nginx,php,www "Help Categories:"
+  echo "Loading help for $MENUCHOICE ..."
+  php ~/bashtools/php_helpers/bash/bash-help.php helptype=$MENUCHOICE
 }
 
 #restarts bash shell
 function bash-restart() {
-	bash-writesettings
-	clear
-	source ~/.bash_profile # this restarts bash as well
+  bash-writesettings
+  clear
+  source ~/.bash_profile # this restarts bash as well
 }
 
 function bash-who() {
-	echo "I am"
-	echo-h1 $welcomemsg
+  echo "I am"
+  echo-h1 $welcomemsg
 }
 
 function bash-logout() {
-	bash-writesettings
-	echo "Written out settings, press enter to exit"
-	read waitb
-	clear
-	source ~/.bash_profile  # this restarts bash as well
+  bash-writesettings
+  echo "Written out settings, press enter to exit"
+  read waitb
+  clear
+  source ~/.bash_profile # this restarts bash as well
 }
 
 # Show history or search in history
 function bash-h() {
-	search=$1
-	if [ "$search" == "" ]; then
-		echo "enter search text"
-		read search
-	fi
-	clear
-	echo "history search:"
-	echo "$search"
-	echo-hr
-	if [ "$search" == "" ]; then
-		history
-	else
-		history | grep $search
-	fi
-	echo-hr
+  search=$1
+  if [ "$search" == "" ]; then
+    echo "enter search text"
+    read search
+  fi
+  clear
+  echo "history search:"
+  echo "$search"
+  echo-hr
+  if [ "$search" == "" ]; then
+    history
+  else
+    history | grep $search
+  fi
+  echo-hr
 }
 
 function bash-sudoers() {
-	grep '^sudo:.*$' /etc/group | cut -d: -f4
+  grep '^sudo:.*$' /etc/group | cut -d: -f4
 }
 
 # displays the current bashtoolscfg files
 function bash-cfg() {
-	echo-b "bash.env"
-	echo ""
-	tail ~/bashtoolscfg/bash.env
-	echo -e "\n"
-	echo-b "os_status"
-	echo ""
-	tail ~/bashtoolscfg/os_status
-	echo -e "\n"
-	echo-b "os_status.env"
-	echo ""
-	tail ~/bashtoolscfg/os_status.env
-	echo -e "\n"
-	echo-b "wwwsites"
-	echo ""
-	tail ~/bashtoolscfg/wwwsites
+  echo-b "bash.env"
+  echo ""
+  tail ~/bashtoolscfg/bash.env
+  echo -e "\n"
+  echo-b "os_status"
+  echo ""
+  tail ~/bashtoolscfg/os_status
+  echo -e "\n"
+  echo-b "os_status.env"
+  echo ""
+  tail ~/bashtoolscfg/os_status.env
+  echo -e "\n"
+  echo-b "wwwsites"
+  echo ""
+  tail ~/bashtoolscfg/wwwsites
 }
 
 function bash-writesettings() {
-	csv=""
-	for i in {0..9}; do
-		csv+="${wwwrepos[$i]},"
-	done
-	#20230629echo $csv;
-	#20250224echo "$csv" >~/bashtoolscfg/wwwsites
-		echo "$csv" >~/bashtoolscfg/wwwrepos
-	echo "$os_status,$sshsecure" >~/bashtoolscfg/os_status
-	echo "$git_ssh" >~/bashtoolscfg/gitcfg
-	echo "$environment,$www_repofocus,$ssh1,$ssh2,$defaultDatabaseIP,$serverid,,$gituname,$phpNo,$ipgateway,$welcomemsg,$wwwroot,$platform,$wwwrepos,$www_repofocus" >~/bashtoolscfg/bash.env
+  csv=""
+  for i in {0..9}; do
+    csv+="${wwwrepos[$i]},"
+  done
+  #20230629echo $csv;
+  #20250224echo "$csv" >~/bashtoolscfg/wwwsites
+  echo "$csv" >~/bashtoolscfg/wwwrepos
+  echo "$os_status,$sshsecure" >~/bashtoolscfg/os_status
+  echo "$git_ssh" >~/bashtoolscfg/gitcfg
+  echo "$environment,$www_repofocus,$ssh1,$ssh2,$defaultDatabaseIP,$serverid,,$gituname,$phpNo,$ipgateway,$welcomemsg,$wwwroot,$platform,$wwwrepos,$www_repofocus" >~/bashtoolscfg/bash.env
 }
 
 function bash-readsettings() {
-	#20250224wwwsites=$(<~/bashtoolscfg/wwwsites})
-	wwwrepos=$(<~/bashtoolscfg/wwwrepos)
+  #20250224wwwsites=$(<~/bashtoolscfg/wwwsites})
+  wwwrepos=$(<~/bashtoolscfg/wwwrepos)
 
-	IFS=', ' read -r -a wwwrepos <<<"$wwwrepos" #read back in same order as written
+  IFS=', ' read -r -a wwwrepos <<<"$wwwrepos" #read back in same order as written
 
-	csv=$(<~/bashtoolscfg/os_status)
-	IFS=', ' read -r -a values <<<"$csv" #read back in same order as written
-	os_status=${values[0]}
-	sshsecure=${values[1]}
+  csv=$(<~/bashtoolscfg/os_status)
+  IFS=', ' read -r -a values <<<"$csv" #read back in same order as written
+  os_status=${values[0]}
+  sshsecure=${values[1]}
 
-	csv=$(<~/bashtoolscfg/gitcfg)
-	IFS=', ' read -r -a values <<<"$csv" #read back in same order as written
-	git_ssh=${values[0]}
+  csv=$(<~/bashtoolscfg/gitcfg)
+  IFS=', ' read -r -a values <<<"$csv" #read back in same order as written
+  git_ssh=${values[0]}
 
-	csv=$(<~/bashtoolscfg/bash.env)
-	IFS=', ' read -r -a values <<<"$csv" #read back in same order as written
-	serverid=${values[5]}
-	environment=${values[0]}
-	www_repofocus=${values[1]}
-	ssh1=${values[2]}
-	ssh2=${values[3]}
-	defaultDatabaseIP=${values[4]}
-	gituname=${values[7]}
-	phpNo=${values[8]}
-	ipgateway=${values[9]}
-	welcomemsg=${values[10]}
-	wwwroot=${values[11]}
-	platform=${values[12]}
-	wwwrepos=${values[13]}
-	www_repofocus=${values[14]}
+  csv=$(<~/bashtoolscfg/bash.env)
+  IFS=', ' read -r -a values <<<"$csv" #read back in same order as written
+  serverid=${values[5]}
+  environment=${values[0]}
+  www_repofocus=${values[1]}
+  ssh1=${values[2]}
+  ssh2=${values[3]}
+  defaultDatabaseIP=${values[4]}
+  gituname=${values[7]}
+  phpNo=${values[8]}
+  ipgateway=${values[9]}
+  welcomemsg=${values[10]}
+  wwwroot=${values[11]}
+  platform=${values[12]}
+  wwwrepos=${values[13]}
+  www_repofocus=${values[14]}
 }
--- bash_modules/bash_modules_windows/git-.sh
+++ bash_modules/bash_modules_windows/git-.sh
@@ -1,66 +1,66 @@
 #!/usr/bin/env bash
 
 function git-installrepo() {
-	env-attributerequire gituname
-	reponame=$1
-	user=$USER
-	rm -R -f $wwwroot/html/$reponame
-	mkdir $wwwroot/html/$reponame
-	git clone git@github.com:$gituname/$reponame.git $wwwroot/html/$reponame
-	git-deploysubrepos
-	cd "$wwwroot/html/$www_repofocus"
-	echo "set focused repo to '$www_repofocus'"
-	bash-writesettings
-	bash-restart
+  env-attributerequire gituname
+  reponame=$1
+  user=$USER
+  rm -R -f $wwwroot/html/$reponame
+  mkdir $wwwroot/html/$reponame
+  git clone git@github.com:$gituname/$reponame.git $wwwroot/html/$reponame
+  git-deploysubrepos
+  cd "$wwwroot/html/$www_repofocus"
+  echo "set focused repo to '$www_repofocus'"
+  bash-writesettings
+  bash-restart
 }
 
 function git-deploysubrepos() {
-	git-deploysubrepo "$www_repofocus/public" "DD_libwww"
-	git-deploysubrepo "$www_repofocus/app" "DD_laravelAp"
-	git-deploysubrepo "$www_repofocus/resources/views" "DD_laraview"
-	echo-hr
+  git-deploysubrepo "$www_repofocus/public" "DD_libwww"
+  git-deploysubrepo "$www_repofocus/app" "DD_laravelAp"
+  git-deploysubrepo "$www_repofocus/resources/views" "DD_laraview"
+  echo-hr
 }
 
 function git-deploysubrepo() {
-	subrepopath=$1
-	subreponame=$2
-	subrepopath="$wwwroot/html/$subrepopath"
-	rm -R $subrepopath/$subreponame
-	echo-hr
-	echo "cloning subrepo $subreponame"
-	echo-hr
-	echo ""
-	git clone git@github.com:$gituname/$subreponame.git $subrepopath/$subreponame
-	echo ""
-	echo "subrepo deployment of $subreponame finished at:"
-	echo-now
-	echo ""
+  subrepopath=$1
+  subreponame=$2
+  subrepopath="$wwwroot/html/$subrepopath"
+  rm -R $subrepopath/$subreponame
+  echo-hr
+  echo "cloning subrepo $subreponame"
+  echo-hr
+  echo ""
+  git clone git@github.com:$gituname/$subreponame.git $subrepopath/$subreponame
+  echo ""
+  echo "subrepo deployment of $subreponame finished at:"
+  echo-now
+  echo ""
 }
 
 function git-pull-repo() {
-	gitreponame=$1
-	if [ "$gitreponame" == "DD_laraview" ]; then
-		gitrepopath="$wwwroot/html/$www_repofocus/resources/views"
-	elif [ "$gitreponame" == "DD_libwww" ]; then
-		gitrepopath="$wwwroot/html/$www_repofocus/public"
-	elif [ "$gitreponame" == "DD_laravelAp" ]; then
-		gitrepopath="$wwwroot/html/$www_repofocus/app"
-	elif [ "$gitreponame" == "bashtools" ]; then
-            		gitrepopath=~
-	else
-		gitrepopath="$wwwroot/html"
-		gitreponame=$www_repofocus
-	fi
+  gitreponame=$1
+  if [ "$gitreponame" == "DD_laraview" ]; then
+    gitrepopath="$wwwroot/html/$www_repofocus/resources/views"
+  elif [ "$gitreponame" == "DD_libwww" ]; then
+    gitrepopath="$wwwroot/html/$www_repofocus/public"
+  elif [ "$gitreponame" == "DD_laravelAp" ]; then
+    gitrepopath="$wwwroot/html/$www_repofocus/app"
+  elif [ "$gitreponame" == "bashtools" ]; then
+    gitrepopath=~
+  else
+    gitrepopath="$wwwroot/html"
+    gitreponame=$www_repofocus
+  fi
 
-	echo-hr
-    echo "pulling repo $gitreponame"
-    echo "to $gitrepopath/$gitreponame;"
-    echo-hr
+  echo-hr
+  echo "pulling repo $gitreponame"
+  echo "to $gitrepopath/$gitreponame;"
+  echo-hr
 
-	cd $gitrepopath/$gitreponame
-	git pull
-	echo ""
-	echo "Finished at:"
-	echo-now
-	echo-hr
+  cd $gitrepopath/$gitreponame
+  git pull
+  echo ""
+  echo "Finished at:"
+  echo-now
+  echo-hr
 }
