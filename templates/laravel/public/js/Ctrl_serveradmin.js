f7917d48ca4b76bea4d126ac582f4efb0945579e
Tim H <tim.higham@liveinfo247.com>
2025-03-20 20:20:01 +0000

    update

--- templates/laravel/public/js/Ctrl_serveradmin.js
+++
@@ -1,9 +0,0 @@
-class Controller extends BaseJS_Ctrl {
-      __subconstruct() {
-      }
-
-      start() {
-	    $("#www_sitefocus_span").html(www_sitefocus);
-      }
-
-}
--- templates/laravel/public/js/Middleware.js
+++
@@ -1,8 +0,0 @@
-class Middleware extends BaseJS_Middleware {
-    __subconstruct() {
-    }
-    widgetaction(){
-        alert("middleware action");
-        return true;
-    }
-}
