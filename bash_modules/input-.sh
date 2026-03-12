b58317ff87e52c5840b1b9733fcda379a8b5287d
Tim H <tim.higham@liveinfo247.com>
2026-03-12 21:52:58 +0000

    update

--- bash_modules/input-.sh
+++ bash_modules/input-.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+#ensure input is not blank
+function input-required() {
+  prompt="$1"
+  if [[ -z "$prompt" ]]; then
+    prompt='Enter a value'
+  fi
+  while true; do
+    # if counter is equal to 2 exit
+    read -p "$prompt : " INPUT
+    if [[ -z "$INPUT" ]]; then
+      echo "Error! Input cannot be empty"
+      continue
+    fi
+    # this lines will be executed only if the conditions passed
+    break
+  done
+  echo "Value : "$INPUT
+}
